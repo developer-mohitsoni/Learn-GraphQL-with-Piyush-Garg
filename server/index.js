180c34d0c71938e16cf95cc3b61b8bc93f0279e9
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-23 18:03:48 +0530

    Add axios dependency for making HTTP requests and refactor our code

--- server/index.js
+++ server/index.js
@@ -4,34 +4,37 @@ const { expressMiddleware } = require("@apollo/server/express4");
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const startServer = async () => {
   const app = express();
 
-  const server = new ApolloServer({
-    typeDefs: `
-        type Todo {
-            id: ID!
-            title: String!
-            completed: Boolean
-        }
+  const typeDefs = `
+  #graphql
+  
+    type Todo {
+      id: ID!
+      title: String!
+      completed: Boolean!
+    }
 
-        type Query {
-            getTodos: [Todo]
-        }
-    `,
-    resolvers: {
-      Query: {
-        getTodos: () => [
-          {
-            id: 1,
-            title: "Hello World",
-            completed: false,
-          },
-        ],
+    type Query {
+      getTodos: [Todo]!
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      getTodos: async () => {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        return response.data;
       },
     },
-  });
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
 
   app.use(bodyParser.json());
   app.use(cors());
